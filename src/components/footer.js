2ba10541158beaa93793f6afc92a5777e4cf1b04
Srushti <srushtideshmukh54@gmail.com>
2023-04-14 22:34:10 +0530

    Apple Clone done again 2

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,13 +5,13 @@ export default function Footer(){
     return(
         <div className={classes.footerRoot}>
             <div className={classes.footerCopyright}>© Copyright 2023 Apple Inc. All rights reserved.</div>
-            <ul className={classes.footerLinks}>
+            {/* <ul className={classes.footerLinks}>
                 <li><a href="https://www.apple.com/in/legal/privacy/">Privacy policy |</a></li>
                 <li><a href="https://www.apple.com/in/legal/internet-services/terms/site.html">Terms of use |</a></li>
                 <li><a href="https://www.apple.com/in/shop/browse/open/salespolicies">Sales Policy |</a></li>
                 <li><a href="https://www.apple.com/in/legal/">Legal |</a></li>
                 <li><a href="https://www.apple.com/in/sitemap/">Site Map</a></li>
-            </ul>
+            </ul> */}
             <div className={classes.footerCountry}>India |
                 
             </div>
